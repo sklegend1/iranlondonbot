3bc092c2ef6af5108fae3145116ac3899d11c06a
sklegend1 <sut.sohrab@gmail.com>
2025-11-06 14:12:24 +0330

    currency service fixed

--- src/infrastructure/queue/workers/currencyWorker.ts
+++ src/infrastructure/queue/workers/currencyWorker.ts
@@ -4,16 +4,24 @@ import { redisOptions } from "../queueClient";
 import { CurrencyService } from "../../external/CurrencyService";
 import { TelegramService } from "../../telegram/TelegramService";
 import { PostCurrencyUpdate } from "../../../application/use-cases/PostCurrencyUpdate";
+import { PrismaBotSettingRepository } from "../../db/repositories/PrismaBotSettingRepository";
 
 require("dotenv").config();
-
+const botSettingRepo = new PrismaBotSettingRepository();
 const currencyService = new CurrencyService(process.env.CURRENCY_API_URL!);
-const telegram = new TelegramService(process.env.TELEGRAM_BOT_TOKEN!, process.env.TELEGRAM_CHANNEL_ID!);
-const postCurrency = new PostCurrencyUpdate(currencyService, telegram);
+
 
 export const currencyWorker = new Worker(
   "currency",
   async (job) => {
+
+    const channelId = (await botSettingRepo.getValue("main_channel"))?.value
+    
+    const telegram = new TelegramService(
+      process.env.TELEGRAM_BOT_TOKEN!,
+      (channelId?.startsWith("@")?channelId:`@${channelId?.toLowerCase()}` ) || process.env.TELEGRAM_CHANNEL_ID!
+    );
+    const postCurrency = new PostCurrencyUpdate(currencyService, telegram);
     console.log(`[CurrencyWorker] job received: ${job.name}`);
     if (job.data.action === "fetch") {
       try {
